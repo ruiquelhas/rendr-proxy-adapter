33f304add8ce0c3c9cb4066dfcf8e2eb66f88003
Rui Quelhas <rfpquelhas@gmail.com>
2014-09-10 22:18:05 +0100

    Add support for cookie subsets with cookie-parser.

--- test/index.js
+++ test/index.js
@@ -6,21 +6,35 @@ var ProxyAdapter = require('../');
 lab.experiment('Rendr proxy adapter', function () {
   //jshint sub: true
 
-  lab.test('keeps request cookies parsed using the deprecated parser', function (done) {
+  lab.test('keeps all request cookies with cookie-parser', function (done) {
     var adapter = new ProxyAdapter();
 
     var api = { method: null }; // avoid the method being reset by rendr
-    var req = { cookies: { name: 'value' }};
+    var req = { cookies: { fst: 'value1', snd: 'value2' }};
 
     var output = adapter.apiDefaults(api, req);
 
     Lab.expect(output.headers).to.have.property('Cookie');
-    Lab.expect(output.headers['Cookie']).to.equal('name=value');
+    Lab.expect(output.headers['Cookie']).to.equal('fst=value1; snd=value2');
 
     done();
   });
 
-  lab.test('keeps the request cookies parsed using the supported parser', function (done) {
+  lab.test('keeps a subset of request cookies with cookie-parser', function (done) {
+    var adapter = new ProxyAdapter({ cookies: ['fst'] });
+
+    var api = { method: null }; // avoid the method being reset by rendr
+    var req = { cookies: { fst: 'value1', snd: 'value2' }};
+
+    var output = adapter.apiDefaults(api, req);
+
+    Lab.expect(output.headers).to.have.property('Cookie');
+    Lab.expect(output.headers['Cookie']).to.equal('fst=value1');
+
+    done();
+  });
+
+  lab.test('keeps a subset of request cookies with connect-cookies', function (done) {
     var adapter = new ProxyAdapter({ cookies: ['name'] });
 
     var api = { method: null }; // avoid the method being reset by rendr
