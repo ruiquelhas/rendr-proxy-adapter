3029f84a1ffcae11e5dd89a326170811253e3efe
Steffen Leistner <sleistner@gmail.com>
2015-06-23 11:48:13 +0200

    Add option to keep headers and cookies as per api configuration.

--- test/index.js
+++ test/index.js
@@ -10,9 +10,35 @@ var ProxyAdapter = require('../');
 lab.experiment('Rendr proxy adapter', function () {
     //jshint sub: true
 
-    lab.test('keeps all request cookies with cookie-parser', function (done) {
+    lab.test('keeps no request cookies', function (done) {
 
         var adapter = new ProxyAdapter();
+
+        var api = {
+            method: null
+        }; // avoid the method being reset by rendr
+        var req = {
+            cookies: {
+                fst: 'value1',
+                snd: 'value2'
+            }
+        };
+
+        var output = adapter.apiDefaults(api, req);
+
+        Code.expect(output.headers).to.not.include('Cookie');
+
+        return done();
+    });
+
+    lab.test('keeps all request cookies with cookie-parser', function (done) {
+
+        var adapter = new ProxyAdapter({
+            default: {
+                host: 'example.com',
+                cookies: ['fst', 'snd']
+            }
+        });
 
         var api = {
             method: null
@@ -35,7 +61,9 @@ lab.experiment('Rendr proxy adapter', function () {
     lab.test('keeps a subset of request cookies with cookie-parser', function (done) {
 
         var adapter = new ProxyAdapter({
-            cookies: ['fst']
+            default: {
+                cookies: ['fst']
+            }
         });
 
         var api = {
@@ -59,7 +87,9 @@ lab.experiment('Rendr proxy adapter', function () {
     lab.test('keeps a subset of request cookies with connect-cookies', function (done) {
 
         var adapter = new ProxyAdapter({
-            cookies: ['name']
+            default: {
+                cookies: ['name']
+            }
         });
 
         var api = {
@@ -85,7 +115,11 @@ lab.experiment('Rendr proxy adapter', function () {
 
     lab.test('keeps the raw request cookies if the parser is not known', function (done) {
 
-        var adapter = new ProxyAdapter();
+        var adapter = new ProxyAdapter({
+            default: {
+                cookies: ['name']
+            }
+        });
         var api = {
             method: null
         }; // avoid the method being reset by rendr
@@ -117,8 +151,15 @@ lab.experiment('Rendr proxy adapter', function () {
 
     lab.test('falls back to the default api options', function (done) {
 
-        var adapter = new ProxyAdapter();
-        var output = adapter.apiDefaults();
+        var adapter = new ProxyAdapter({
+            default: {
+                host: 'example.com'
+            }
+        });
+        var api = {
+            method: null
+        }; // avoid the method being reset by rendr
+        var output = adapter.apiDefaults(api);
 
         Code.expect(output.headers).to.be.include('User-Agent');
 
@@ -129,6 +170,33 @@ lab.experiment('Rendr proxy adapter', function () {
 
         var adapter = new ProxyAdapter({
             headers: ['host']
+        });
+
+        var api = {
+            method: null
+        }; // avoid the method being reset by rendr
+
+        var req = {
+            headers: {
+                'host': 'localhost:1337'
+            }
+        };
+
+        var output = adapter.apiDefaults(api, req);
+
+        Code.expect(output.headers).to.include('host');
+        Code.expect(output.headers).to.not.include('hostname');
+        Code.expect(output.headers['host']).to.equal('localhost:1337');
+
+        return done();
+    });
+
+    lab.test('keeps the selected request headers', function (done) {
+
+        var adapter = new ProxyAdapter({
+            default: {
+                headers: ['host']
+            }
         });
 
         var api = {
